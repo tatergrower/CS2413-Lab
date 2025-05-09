753ecf55c2924ddcf9a63bd89c1ff5e78cf2bbed
tatergrower <cjcempura@gmail.com>
2025-05-08 23:22:57 -0500

    Update Lab4Symmetry.c

--- Lab4Symmetry.c
+++ Lab4Symmetry.c
@@ -1,1 +1,24 @@
 
+// checks if trees are mirrors images
+bool isMirror(struct TreeNode* n1, struct TreeNode* n2) {
+    if (n1 == NULL && n2 == NULL) {
+        return true;
+    }
+
+    if (n1 == NULL || n2 == NULL) {
+        return false;
+    }
+
+    return (n1->val == n2->val) &&
+           isMirror(n1->left, n2->right) &&
+           isMirror(n1->right, n2->left);
+}
+
+//main function that actually checks symmetry
+bool isSymmetric(struct TreeNode* root) {
+    if (root == NULL) {
+        return true;
+    }
+    return isMirror(root->left, root->right);
+}
+
