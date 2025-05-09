c9f5f445c8b7a75b74a71c650b008d36c3fec8c5
tatergrower <cjcempura@gmail.com>
2025-05-08 23:28:48 -0500

    Update Lab4Islands.c

--- Lab4Islands.c
+++ Lab4Islands.c
@@ -1,1 +1,28 @@
+void dfs(char** grid, int gridSize, int* gridColSize, int i, int j) {
+    if (i < 0 || i >= gridSize || j < 0 || j >= gridColSize[i] || grid[i][j] == '0') {
+        return;
+    }
+
+    grid[i][j] = '0'; // Mark as visited
+
+    dfs(grid, gridSize, gridColSize, i + 1, j);
+    dfs(grid, gridSize, gridColSize, i - 1, j);
+    dfs(grid, gridSize, gridColSize, i, j + 1);
+    dfs(grid, gridSize, gridColSize, i, j - 1);
+}
+
+int numIslands(char** grid, int gridSize, int* gridColSize) {
+    int count = 0;
+
+    for (int i = 0; i < gridSize; i++) {
+        for (int j = 0; j < gridColSize[i]; j++) {
+            if (grid[i][j] == '1') {
+                count++;            // Only increment when we find a new island
+                dfs(grid, gridSize, gridColSize, i, j);
+            }
+        }
+    }
+
+    return count;
+}
 
