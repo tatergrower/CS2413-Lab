d847e72f696daa154c552d922dba9f369dff241c
tatergrower <cjcempura@gmail.com>
2025-05-08 23:40:20 -0500

    Update Lab4Islands.c

--- Lab4Islands.c
+++ Lab4Islands.c
@@ -1,19 +1,20 @@
 void dfs(char** grid, int gridSize, int* gridColSize, int i, int j) {
+    //bound check
     if (i < 0 || i >= gridSize || j < 0 || j >= gridColSize[i] || grid[i][j] == '0') {
         return;
     }
-
-    grid[i][j] = '0'; // Mark as visited
-
-    dfs(grid, gridSize, gridColSize, i + 1, j);
-    dfs(grid, gridSize, gridColSize, i - 1, j);
-    dfs(grid, gridSize, gridColSize, i, j + 1);
-    dfs(grid, gridSize, gridColSize, i, j - 1);
+    grid[i][j] = '0'; // marks as visited by converting 1 to 0
+    
+    //recursively checks all possible directions
+    dfs(grid, gridSize, gridColSize, i + 1, j);//down
+    dfs(grid, gridSize, gridColSize, i - 1, j);//up
+    dfs(grid, gridSize, gridColSize, i, j + 1);//right
+    dfs(grid, gridSize, gridColSize, i, j - 1);//left
 }
-
+//counts the islands, intiats dfs from every unvisted land 
 int numIslands(char** grid, int gridSize, int* gridColSize) {
     int count = 0;
-
+    //iterates through cells
     for (int i = 0; i < gridSize; i++) {
         for (int j = 0; j < gridColSize[i]; j++) {
             if (grid[i][j] == '1') {
@@ -22,7 +23,6 @@ int numIslands(char** grid, int gridSize, int* gridColSize) {
             }
         }
     }
-
-    return count;
+ return count;
 }
 
